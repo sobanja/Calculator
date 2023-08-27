c799390c067b765ee9ea63cb1804960d0c69be58
Ana Volenter <anavolenter1@gmail.com>
2023-08-28 00:10:38 +0300

    add new comments

--- calc.js
+++ calc.js
@@ -1,12 +1,12 @@
-// значение, которое выводится на странице
+// Получение элементов, на которых будет отображаться выражение и результат
 const expressionElement = document.getElementById("expression");
 const resultElement = document.getElementById("result");
-// цифры
+
+// Получение кнопок для цифр, операторов и скобок
 const numberButtons = document.querySelectorAll(".number");
-// операторы
 const operatorButtons = document.querySelectorAll(".operator");
-// скобки
 const bracketButtons = document.querySelectorAll(".bracket");
+
 // кнопка равно
 const calculateButton = document.getElementById("calculate");
 // clear btn
@@ -16,13 +16,17 @@ const ac = document.querySelector(".ac");
 let expression = "";
 expressionElement.textContent = "0";
 
+// Назначение обработчиков событий для кнопок цифр
 numberButtons.forEach(button => {
   button.addEventListener("click", () => {
+    // при нажатии кнопок цифр, знач будет заноситься в переменную выражения
+    // и на странице в контенте
     expression += button.textContent;
     expressionElement.textContent = expression;
   });
 });
 
+// Назначение обработчиков событий для кнопок операторов
 operatorButtons.forEach(button => {
   button.addEventListener("click", () => {
     expression += " " + button.textContent + " ";
@@ -30,6 +34,7 @@ operatorButtons.forEach(button => {
   });
 });
 
+// Назначение обработчиков событий для кнопок скобок
 bracketButtons.forEach(button => {
   button.addEventListener("click", () => {
     expression += " " + button.textContent + " ";
@@ -37,6 +42,7 @@ bracketButtons.forEach(button => {
   });
 });
 
+// Назначение обработчика события для кнопки "Calculate" =
 calculateButton.addEventListener("click", () => {
   const result = evaluateExpression(expression);
   resultElement.textContent = result;
@@ -50,20 +56,24 @@ function clearAll() {
   expression = '';
 }
 
+// Назначение обработчика события на кнопку "AC" для очистки
 document.querySelector(".ac").onclick = clearAll;
 
+// Функция для вычисления выражения
 function evaluateExpression(expression) {
   try {
-    // Implementing a simple stack-based expression evaluator
+     // Преобразование выражения в массив токенов
     const tokens = expression.split(" ");
     const stack = [];
 
+    // Обработка каждого токена в выражении
     for (const token of tokens) {
       if (!isNaN(token)) {
         stack.push(parseFloat(token));
       } else if (token === "(") {
         stack.push(token);
       } else if (token === ")") {
+        // Обработка закрывающей скобки
         let subExpression = [];
         while (stack.length > 0 && stack[stack.length - 1] !== "(") {
           subExpression.unshift(stack.pop());
@@ -74,17 +84,17 @@ function evaluateExpression(expression) {
         stack.push(token);
       }
     }
-
+    // Вычисление подвыражений и операций
     return evaluateSubExpression(stack);
   } catch (error) {
     return "Error";
   }
 }
-
+// Функция для вычисления подвыражения
 function evaluateSubExpression(subExpression) {
   const operators = [];
   const operands = [];
-
+// Обработка токенов в подвыражении
   for (const token of subExpression) {
     if (!isNaN(token)) {
       operands.push(token);
@@ -98,7 +108,7 @@ function evaluateSubExpression(subExpression) {
       operators.push(token);
     }
   }
-
+ // Выполнение оставшихся операций
   while (operators.length > 0) {
     const operator = operators.pop();
     const operand2 = operands.pop();
@@ -109,6 +119,7 @@ function evaluateSubExpression(subExpression) {
   return operands[0];
 }
 
+// Определение приоритета операторов
 function precedence(operator) {
   if (operator === "+" || operator === "-") {
     return 1;
@@ -117,7 +128,7 @@ function precedence(operator) {
   }
   return 0;
 }
-
+// Применение оператора к операндам
 function applyOperator(operator, operand1, operand2) {
   switch (operator) {
     case "+":
